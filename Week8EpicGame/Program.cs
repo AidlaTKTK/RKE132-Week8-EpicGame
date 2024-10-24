c2edd5a0a3a61044245f9bc64e08ff129fa4411f
Markus Aidla <markus.aidla@tktk.ee>
2024-10-24 20:05:51 +0300

    add code

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+string folderPath = @"C:\data\";
+string heroFile = "Heroes.txt";
+string villainFile = "Villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath, heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+string[] weapon = { "Mace", "Dodgeball", "Stick", "Gun", "Boxing gloves" };
+
+
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+int heroHP = GetCharacterHP(hero);
+int heroStrikeStrength = heroHP;
+Console.WriteLine($"Today {hero} ({heroHP}) with {heroWeapon} saves the day!");
+
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+int villainHP = GetCharacterHP(villain);
+int villainStrikeStrength = villainHP;
+Console.WriteLine($"Today {villain} ({villainHP}) with {villainWeapon} tries to take over the world");
+
+
+while(heroHP > 0 && villainHP > 0)
+{
+    heroHP = heroHP - Hit(villain, villainStrikeStrength);
+    villainHP = villainHP - Hit(hero, heroStrikeStrength);
+}
+
+Console.WriteLine($"Hero {hero} HP: {heroHP}");
+Console.WriteLine($"Villain {villain} HP: {villainHP}");
+
+if (heroHP > 0)
+{
+    Console.WriteLine($"{hero} wins!");
+}
+else if (villainHP > 0)
+{
+    Console.WriteLine("Villain wins...");
+}
+else
+{
+    Console.WriteLine("Draw!");
+}
+
+
+static string GetRandomValueFromArray (string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
+
+static int GetCharacterHP(string characterName)
+{
+    if(characterName.Length < 10)
+    {
+        return 10;
+    }
+    else
+    {
+        return characterName.Length;
+    }
+}
+
+static int Hit(string characterName, int characterHP)
+{
+    Random rnd = new Random();
+    int strike = rnd.Next(0, characterHP);
+
+    if(strike == 0)
+    {
+        Console.WriteLine($"{characterName} missed.");
+        return strike;
+    }
+    else if (strike == characterHP - 1)
+    {
+        Console.WriteLine($"{characterName} got a critical hit.");
+    }
+    else
+    {
+        Console.WriteLine($"{characterName} got a hit of {strike}");
+    }
+
+    return strike;
+}
